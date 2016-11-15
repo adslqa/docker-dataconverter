edb7ffd68dbe6c5619761c3736df337f0390a017
davemetocean <dave@ngaru.rag.metocean.co.nz>
2016-11-15 15:43:03 +1300

    Working with fcron

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN yum -y install epel-release &&\
     yum install -y deltarpm &&\
     yum update -y &&\
     yum install -y wget curl libcurl curl-devel subversion git tar gzip unzip \
-                gcc gcc-c++ gfortran \
+                gcc gcc-c++ gcc-gfortran \
                 make cmake cmake3 autoconf automake libtool \
                 libgcc \
                 glibc glibc-devel glibc-static \
@@ -17,15 +17,12 @@ RUN yum -y install epel-release &&\
                 freetype freetype-devel libpng libpng-devel libtiff libtiff-devel openjpeg openjpeg-devel \
                 perl-devel python-devel libffi devel-libffi libpng12 \
                 lsof net-tools sysstat lsyncd &&\
-    yum install -y netcdf netcdf-devel hdf5 hdf hdf-devel \
-               netcdf4-python geos-devel \
-               openblas openblas-devel \
-               lapack lapack-devel lapack-static \
-               blas-devel blas-static \
-               openssl-devel libffi-devel python-devel python-pip &&\
     yum clean all
 
+RUN systemctl enable lsyncd.service
 
 USER root
 ADD ./ /tmp/install
 RUN /tmp/install/install.sh
+
+RUN systemctl enable fcron.service
--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 set -ev
 
-echo '--- installing pip install ----'
-pip install --upgrade pip
-pip install requests[security]
-export PIP_DEFAULT_TIMEOUT=300
+echo "-----------------Install fcron ------------------------------"
+cd /tmp
+curl -L -o fcron.tar.gz http://fcron.free.fr/archives/fcron-3.2.0.src.tar.gz
+tar -zxf fcron.tar.gz
+cd fcron-3.2.0
+./configure --prefix=/usr --sysconfdir=/etc --without-sendmail
+make
+make install
+
 
 echo "-----------------Install WGRIB2-----------------"
 cd /tmp
@@ -17,44 +22,5 @@ mv wgrib2 /usr/local/
 ln -s /usr/local/wgrib2/wgrib2 /usr/local/bin/
 cd "$BASEDIR"
 
-echo "-----------------Install GRIB-API-----------------"
-cd /tmp
-curl -o grib_api_src.tar.gz https://software.ecmwf.int/wiki/download/attachments/3473437/grib_api-1.14.7-Source.tar.gz
-tar xfz grib_api_src.tar.gz
-cd grib_api-1.14.7-Source
-./configure
-make
-make install
-
-echo '--------------- installing proj ---------------'
-cd /tmp
-curl -L -O http://download.osgeo.org/proj/proj-4.9.3.tar.gz
-tar zxvf proj-4.9.3.tar.gz && cd proj-4.9.3
-./configure 2>&1 | tee configure.log
-make 2>&1 | tee make.log
-make check 2>&1 | tee check.log
-make install 2>&1 | tee install.log
-ldconfig
-
-echo "-----------------Install CDO-----------------"
-cd /tmp
-curl -L -o cdo.tar.gz https://code.zmaw.de/attachments/download/12760/cdo-1.7.2.tar.gz
-tar -zxf cdo.tar.gz
-cd cdo-1.7.2
-#./configure --with-netcdf=/usr --with-hdf5=/usr --prefix=/usr/local
-./configure --with-netcdf=yes --with-hdf5=yes --with-grib_api=yes --with-proj=/usr/local/share/proj --prefix=/usr/local
-make
-make install
-ldconfig
-
-echo "-----------------Install pip / python stuff -----------------"
-pip install --upgrade numpy
-pip install --upgrade pyyaml
-pip install --upgrade pyproj
-pip install --upgrade pygrib
-pip install --upgrade netcdf4
-pip install --upgrade apscheduler
-
 echo '--- cleaning up ----'
 rm -rf /tmp/*
-rm -rf ~/.cache/pip/*
